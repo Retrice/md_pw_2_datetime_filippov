cee3a97587ff54411fc603c0afd5ee56f5e1df1b
Retrice <@DESKTOP-7RJVNDS>
2025-01-27 01:14:42 +0300

    first commit

---
+++ bin/md_pw_2_datetime_filippov.dart
@@ -0,0 +1,5 @@
+import 'package:md_pw_2_datetime_filippov/md_pw_2_datetime_filippov.dart' as md_pw_2_datetime_filippov;
+
+void main(List<String> arguments) {
+  print('Hello world: ${md_pw_2_datetime_filippov.calculate()}!');
+}
---
+++ test/md_pw_2_datetime_filippov_test.dart
@@ -0,0 +1,8 @@
+import 'package:md_pw_2_datetime_filippov/md_pw_2_datetime_filippov.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
